a66562986bb3a9e5f857be737d320040bc414e00
Jackson Benete <jacksonbenete@gmail.com>
2021-02-15 20:24:27 -0300

    Componente SendMessageButton

--- frontend/src/components/ChatRoom/SendMessageButton.tsx
+++ frontend/src/components/ChatRoom/SendMessageButton.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react'
 import {Button, Icon, IconButton} from '@material-ui/core'
-// import styled from 'styled-components'
+import svgString  from './SendMessageButton.svg'
+import styled from 'styled-components'
 
 /** Exemplo de como passar typado */
 // interface ButtonProps {
@@ -12,14 +13,16 @@ import {Button, Icon, IconButton} from '@material-ui/core'
 //     )
 // }
 
+const SendIcon = styled.span`
+    background: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z"></path></svg>');
+    width: 24px;
+    height: 24px;
+`
+
 const SendMessageButton: FC<{}> = () => {
     return (
         <IconButton>
-            <span data-testid="send" data-icon="send">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="currentColor" d="M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z"></path>
-                </svg>
-            </span>
+            <SendIcon data-testid="send" data-icon="send"></SendIcon>
         </IconButton>
     )
 }
