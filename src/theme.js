f7cf95f4a07a5efebdb9dd12165c9ee7d780e95c
Itacir Gabral <itacirgabral@gmail.com>
2021-02-16 12:36:58 -0400

    materiaui theme

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { green, red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    secondary: red,
+  },
+});
+
+export default theme;
