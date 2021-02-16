a17be207484638517b035db98f3c0e3b8617ee21
Itacir Gabral <itacirgabral@gmail.com>
2021-02-16 17:30:08 -0400

    usando theme.palette

--- src/components/ChatNoChoice/index.jsx
+++ src/components/ChatNoChoice/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-const ChatNoChoice = () => <p>Mantenha o celular carregando</p>;
+const useStyles = makeStyles((theme) => ({
+  primary: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  secondary: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
+
+const ChatNoChoice = () => {
+  const classes = useStyles();
+  return (
+    <p>
+      <span className={classes.primary}>Mantenha o cel</span>
+      <span className={classes.secondary}>ular carregando</span>
+    </p>
+  );
+};
 
 export default ChatNoChoice;
