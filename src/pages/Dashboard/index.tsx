c2167189d67932c23eccaba285a48dd0961ce33f
Jackson Benete <jacksonbenete@gmail.com>
2021-02-17 15:35:01 -0300

    Dashboard layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {FC, useReducer} from 'react';
+import { History } from 'history';
 
-const Dashboard: React.FC<{}> = () => (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import ChatList from '../../components/ChatList/ChatList';
+import { Row, Side, Main, Container, MainContainer, MainContainerIcon, MainContainerTextBox, FirstText, SecondText } from './DashboardStyle'
+
+import { chats } from '../../components/ChatList/FakeDatabase'
+
+interface DashboardProps {
+    history: History,
+    chatId: string
+}
+
+const Dashboard: React.FC<DashboardProps> = ({ chatId, history }) => (
+    <Row>
+      <Side data-testid="Side">
+        <ChatList chatListData={chats} />
+      </Side>
+      <Main data-testid="Main">
+        <Container data-testid="DashboardContainer">
+        
+        <MainContainer>
+            <MainContainerIcon />
+            <FirstText>Keep your phone connected</FirstText>
+            <SecondText>WhatsApp connects to your phone to sync messages. To reduce data usage, connect your phone to Wi-Fi.</SecondText>
+        </MainContainer>
+
+          </Container>
+      </Main>
+    </Row>
 );
 
 export default Dashboard;
