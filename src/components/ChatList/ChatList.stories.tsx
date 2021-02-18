e716d7e59a2f1050025010f517bb07aac03ff6dc
Jackson Benete <jacksonbenete@gmail.com>
2021-02-18 20:52:10 -0300

    ChatList receiving datazero

--- src/components/ChatList/ChatList.stories.tsx
+++ src/components/ChatList/ChatList.stories.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ChatList from './ChatList'
-import { chats } from './FakeDatabase'
+// import React from 'react'
+// import ChatList from './ChatList'
+// import { chats } from './FakeDatabase'
+// import History from 'history'
 
-export default {
-    title: "Componentes Gerais",
-    component: ChatList
-}
+// export default {
+//     title: "Componentes Gerais",
+//     component: ChatList
+// }
 
-export const BasicChatList = () => (
-    <ChatList chatListData={chats}></ChatList>
-)
+// export const BasicChatList = () => (
+//     <ChatList chatListData={chats} history={history}></ChatList>
+// )
 
-BasicChatList.storyName = "Lista de Contatos"
+// BasicChatList.storyName = "Lista de Contatos"
+
+export {}
---
+++ src/components/ChatList/index.tsx
@@ -0,0 +1,71 @@
+import React, {FC} from 'react'
+import moment from 'moment'
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem'
+import Divider from '@material-ui/core/Divider';
+import { DivToolbar, ChatPicture, ChatInfo, ChatName, MessageContent, MessageDate, Container } from './style'
+
+interface ChatListProps {
+    chatListData: {
+        prop1: String,
+        // contacts: {
+        //     name: String,
+        //     status: String,
+        //     avatar: String,
+        // },
+        // chats: Array<any>
+        contacts: any,
+        chats: any
+    };
+}
+
+const ChatList: FC<ChatListProps> = ({ chatListData = {contacts: {}, chats: {}} }) => {
+    const contactList = Object.entries(chatListData.contacts)
+    const messageList = Object.entries(chatListData.chats)
+
+    let chatData = contactList.map((item, i) => Object.assign(
+        {}, 
+        {id: item[0]}, 
+        {info: item[1]}, 
+        {messages: messageList[i][1]}
+    ))
+
+    return(
+        <Container data-testid="ChatDiv">
+                <DivToolbar />
+                <Divider />
+                <List>
+                    {chatData.map((contact: any) => (
+                    <ListItem
+                        key={contact.id}
+                        data-testid="chat"
+                        button
+                        // onClick={navToChat.bind(null, chat)}
+                        >
+                        <ChatPicture
+                        data-testid="picture"
+                        src={contact.info.avatar}
+                        alt="Profile"
+                        />
+                        <ChatInfo>
+                        <ChatName data-testid="name">{contact.info.name}</ChatName>
+                        {contact.messages && (
+                            <React.Fragment>
+                            <MessageContent data-testid="content">
+                                {contact.messages[0].msg}
+                            </MessageContent>
+                            <MessageDate data-testid="date">
+                                {moment.unix(contact.messages[0].timestamp).format('HH:mm')}
+                            </MessageDate>
+                            </React.Fragment>
+                        )}
+                        </ChatInfo>
+                    </ListItem>
+                    ))}
+                </List>
+            </Container>
+    )
+}
+
+export default ChatList
