40631ded504c24133a31e099334048f85d9bf7d3
Jackson Benete <jacksonbenete@gmail.com>
2021-02-18 20:53:53 -0300

    Dashboard receiving datazero

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,44 +1,26 @@
 import React, {FC, useEffect, useReducer} from 'react';
 import { History } from 'history';
 
-import ChatList from '../../components/ChatList/ChatList';
-import { Row, Side, Main, Container, MainContainer, MainContainerIcon, FirstText, SecondText } from './DashboardStyle'
+import ChatList from '../../components/ChatList';
+import { Row, Side, Main } from './style'
 
-import reducer from '../../store/reducers'
-import { ACTIONS } from '../../store/actions'
+import DashboardRoom from '../../components/DashboardRoom';
+
+import datazero from '../../store/datazero'
 
 interface DashboardProps {
     history: History;
 }
 
-const initialState = {
-  chatListData: []
-}
-
-// quer que feche e abra outra vez? nao sei se era o que tu queria
-// pera ae
-const Dashboard: React.FC<DashboardProps> = ({ history }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  useEffect(() => {
-    dispatch({ type: ACTIONS.GET_CHAT })
-  }, [])
+const Dashboard: React.FC<DashboardProps> = ({ }) => {
 
   return (
     <Row>
       <Side data-testid="Side">
-        <ChatList chatListData={[]} history={history} />
+        <ChatList chatListData={datazero} />
       </Side>
       <Main data-testid="Main">
-        <Container data-testid="DashboardContainer">
-        
-        <MainContainer>
-            <MainContainerIcon />
-            <FirstText>Keep your phone connected</FirstText>
-            <SecondText>WhatsApp connects to your phone to sync messages. To reduce data usage, connect your phone to Wi-Fi.</SecondText>
-        </MainContainer>
-
-          </Container>
+        <DashboardRoom />
       </Main>
     </Row>
 )};
---
+++ src/pages/Dashboard/style.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+`
+
+export const Side = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 0.85
+`
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 2
+`
