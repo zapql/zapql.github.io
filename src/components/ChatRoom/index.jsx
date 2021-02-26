25a5eb37d8958234290419ab680eef9017ae682f
Jackson Benete <jacksonbenete@gmail.com>
2021-02-26 19:05:59 -0300

    Fim de implementacao do ciclo de error

--- src/components/ChatRoom/index.jsx
+++ src/components/ChatRoom/index.jsx
@@ -18,49 +18,57 @@ const disableMessageInput = (bool) => {
 const handleError = (error, dispatch, chatId) => {
     disableMessageInput(false)
 
-    console.log("Send Message Error: ", error)
-
-    if (error.type === null) {
-        // TODO: Generate Log
+    try {
+        if (error.type === null) {
+            throw new Error("Retorno nulo de mutation.")
+        }
+        if (error.networkError) {
+            throw new Error("Network Error: the entire query was rejected.")
+        }
+        if (error.graphQLErrors) {
+            throw new Error("GrapgQL Error: failed resolvers on parse, validation or execution phase.")
+        }
+        throw new Error("Unknow Error")
+    } catch (throwedError) {
+        console.error(throwedError)
+        console.table(error)
+        // TODO: log error com mutation em database de erros
+    } finally {
+        dispatch((previousState) => {
+            return ( 
+                {...previousState, 
+                    chats: {
+                        ...previousState.chats,
+                        [chatId]:
+                            [
+                                ...previousState.chats[chatId],
+                                {
+                                    wid: `error-${Math.random()}`,
+                                    msg: "Sua mensagem não pôde ser enviada.",
+                                    isError: true,
+                                    timestamp: (Date.now() / 1000)
+                                }
+                            ]
+                        
+                    }}
+            )
+        })
     }
-    if (error.networkError) {
-        // TODO: Generate Log
-    }
-    if (error.graphQLErrors) {
-        // TODO: Generate Log
-    }
-
-    dispatch((previousState) => {
-        return ( 
-            {...previousState, 
-                chats: {
-                    ...previousState.chats,
-                    [chatId]:
-                        [
-                            ...previousState.chats[chatId],
-                            {
-                                wid: `error-${Math.random()}`,
-                                msg: "Sua mensagem não pôde ser enviada.",
-                                isError: true,
-                                timestamp: (Date.now() / 1000)
-                            }
-                        ]
-                    
-                }}
-        )
-    })
 }
 
 const ChatRoom = ({ chatRoomData = {id:"", info: {}, messages: []}, dispatch }) => {
 
     const [ inputState, setInputState ] = useState('');
 
-    const [ sendMessageMutation, { loading, error, data: returnData } ] = useMutation(SEND_TEXT_MESSAGE_MUTATION)
+    /**
+     * Esse { onError: () => {} } é hack para lidar com uma inconsistência no Apollo.
+     * O fato de retornar o "error" do ciclo da mutaćão deveria ser suficiente para tratar os erros, 
+     * por uma inconsistência (ou por design) sem o onError por enquanto a página quebra e o aplicativo morre.
+     * Ver issue: https://github.com/apollographql/apollo-client/issues/6070
+     */
+    const [ sendMessageMutation, { loading, error, data: returnData } ] = useMutation(SEND_TEXT_MESSAGE_MUTATION, { onError: () => {} })
 
-    // useEffect para useMutation
     useEffect(() => {
-        let chatId = chatRoomData.id
-
         if (loading) {
             disableMessageInput(true)
         }
@@ -69,11 +77,11 @@ const ChatRoom = ({ chatRoomData = {id:"", info: {}, messages: []}, dispatch }) 
         }
         if (returnData) {
             if (returnData[0] === null) return handleError({type: null}, dispatch, chatRoomData.id)
-            
-            disableMessageInput(false)
+
             // TODO: depois vai ter outros tipos de mensagem
             let typeKey = Object.keys(returnData)[0]
 
+            disableMessageInput(false)
             setInputState(() => '')
 
             dispatch((previousState) => {
@@ -81,9 +89,9 @@ const ChatRoom = ({ chatRoomData = {id:"", info: {}, messages: []}, dispatch }) 
                     {...previousState, 
                         chats: {
                             ...previousState.chats,
-                            [chatId]:
+                            [chatRoomData.id]:
                                 [
-                                    ...previousState.chats[chatId],
+                                    ...previousState.chats[chatRoomData.id],
                                     {
                                         ...returnData[typeKey]
                                     }
