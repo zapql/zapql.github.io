7211663cb486ac3ac82dcff5404998020cd5b65a
Jackson Benete <jacksonbenete@gmail.com>
2021-03-02 17:43:01 -0300

    Remove arquivos antigos Typescript

--- oldSrcTypescript/components/ChatRoom/style.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  background: url(/assets/chat-background.jpg);
-  display: flex;
-  flex-flow: column;
-  height: 100vh;
-`;
--- oldSrcTypescript/store/actions/index.ts
+++
@@ -1,9 +0,0 @@
-// import x from './x'
-
-// export {
-//     x
-// }
-
-export const ACTIONS = {
-    GET_CHAT: 'get-chat'
-}
