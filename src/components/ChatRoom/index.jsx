df9c3f975b008ba7eb37b2ccb365d2fd0fd9aedf
Jackson Benete <jacksonbenete@gmail.com>
2021-03-03 15:35:06 -0300

    Tratamento de erros melhorado

--- src/components/ChatRoom/index.jsx
+++ src/components/ChatRoom/index.jsx
@@ -7,75 +7,14 @@ import MessageList from './MessageList';
 import MessageInput from './MessageInput';
 import { Container } from './style'
 
+import { handleError } from '../../services/Errors'
+
 import { useMutation, useSubscription } from '@apollo/client'
 import { SEND_TEXT_MESSAGE_MUTATION, ON_TEXT_MESSAGE_SUBSCRIPTION } from '../../store/Apollo/ChatRoom'
 
 const disableMessageInput = (bool) => {
     document.getElementById("message-input").disabled = bool
     document.getElementById("send-button").disabled = bool
-}
-
-const handleError = (error, dispatch, chatId) => {
-    disableMessageInput(false)
-
-    function ReturnNullException (message, e) {
-        this.message = message
-        this.name = 'ReturnNullException'
-        this.tooltip = 'Return Null Error'
-        this.error = e
-    }
-
-    function NetworkErrorException (message, e) {
-        this.message = message
-        this.name = 'NetworkErrorException'
-        this.tooltip = 'Network Error'
-        this.error = e
-    }
-
-    function GraphQLErrorException (message, e) {
-        this.message = message
-        this.name = 'GraphQLErrorException'
-        this.tooltip = 'GraphQL Error'
-        this.error = e
-    }
-
-    try {
-        if (error.type === null) {
-            throw new ReturnNullException("Retorno nulo de mutation.", error)
-        }
-        if (error.networkError) {
-            throw new NetworkErrorException("Network Error: the entire query was rejected.", error)
-        }
-        if (error.graphQLErrors) {
-            throw new GraphQLErrorException("GraphQL Error: failed resolvers on parse, validation or execution phase.", error)
-        }
-        throw new Error("Unknow Error")
-    } catch (Exception) {
-        console.error(Exception.message)
-        console.table(Exception)
-
-        dispatch((previousState) => {
-            return ( 
-                {...previousState, 
-                    chats: {
-                        ...previousState.chats,
-                        [chatId]:
-                            [
-                                ...previousState.chats[chatId],
-                                {
-                                    wid: `error-${Math.random()}`,
-                                    msg: "Sua mensagem não pôde ser enviada.",
-                                    isError: true,
-                                    errorType: Exception.tooltip,
-                                    timestamp: (Date.now() / 1000)
-                                }
-                            ]
-                        
-                    }}
-            )
-        })
-        // TODO: log error com mutation em database de erros
-    }
 }
 
 const ChatRoom = ({ chatRoomData = {id:"", info: {}, messages: []}, dispatch }) => {
@@ -104,15 +43,16 @@ const ChatRoom = ({ chatRoomData = {id:"", info: {}, messages: []}, dispatch }) 
             disableMessageInput(true)
         }
         if (mutationError) {
-            return handleError(mutationError, dispatch, chatRoomData.id)
+            return handleError(mutationError, dispatch, chatRoomData.id, disableMessageInput(false))
         }
         if (mutationReturnData) {
             // TODO: depois vai ter outros tipos de mensagem
             let typeKey = Object.keys(mutationReturnData)[0]
-            let chatId = mutationReturnData[typeKey].to
 
-            if (mutationReturnData[0] === null) return handleError({type: null}, dispatch, chatRoomData.id)
-            if (mutationReturnData[typeKey] === null) return handleError({type: typeKey, return: null}, dispatch, chatRoomData.id)
+            if (mutationReturnData[0] === null) return handleError({type: null}, dispatch, chatRoomData.id, disableMessageInput(false))
+            if (mutationReturnData[typeKey] === null) return handleError({type: null}, dispatch, chatRoomData.id, disableMessageInput(false))
+
+            let chatId = mutationReturnData[typeKey].to
 
             disableMessageInput(false)
             setInputState({
