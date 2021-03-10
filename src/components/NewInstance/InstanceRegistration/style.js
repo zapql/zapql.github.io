584ddaf026afeee7d1f6aae8e5652c30264a2008
Jackson Benete <jacksonbenete@gmail.com>
2021-03-10 20:56:49 -0300

    InstanceRegistration first commit

---
+++ src/components/NewInstance/InstanceRegistration/style.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import List from '@material-ui/core/List';
+import Button from '@material-ui/core/Button'
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex: 2;
+  max-height: 100vh;
+  text-align: center;
+`
+
+export const PanelTOC = styled.div`
+  display: block;
+  flex: 2;
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding: 0 14vh;
+
+  margin: 5vh;
+  max-height: 50vh;
+
+  scrollbar-color: rgba(0,0,0,.2) hsla(0,0%,100%,.1);
+  scrollbar-width: thin;
+
+  ::-webkit-scrollbar {
+    width: 6px!important;
+    height: 6px!important;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(0,0,0,.2);
+  }
+
+  ::-webkit-scrollbar-track {
+    background: hsla(0,0%,100%,.1);
+  }
+}
+`;
+
+export const IButton = styled(Button)`
+    justify-content: flex-end;
+    display: flex;
+`
---
+++ src/components/NewInstance/InstanceRegistration/toc-example.jsx
@@ -0,0 +1,42 @@
+import Typography from '@material-ui/core/Typography'
+import React from 'react'
+
+const TOC = () => {
+    return (
+        <React.Fragment>
+            <Typography variant="h3" gutterBottom>Lorem Ipsum</Typography>
+
+            <Typography variant="subtitle1" gutterBottom>
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+            </Typography>
+
+            <Typography variant="caption" display="block" gutterBottom>
+                "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+            </Typography>
+
+            <Typography variant="body2" gutterBottom>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend bibendum orci mattis venenatis. Duis non mattis arcu, id finibus diam. Sed fringilla sodales elementum. Phasellus non ipsum quis ligula interdum placerat ut eu libero. Sed vel iaculis quam. Aenean molestie ante ac facilisis convallis. Curabitur consectetur suscipit nibh vel euismod. Donec convallis augue enim, quis dapibus ex dictum molestie. Sed in diam vel massa tincidunt aliquet at sed leo. Sed vulputate turpis sapien, eu pretium nunc aliquet vulputate. Vestibulum quis elit ac justo vulputate venenatis. Integer est elit, tristique nec mi eu, tempus tristique ligula.
+
+                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum diam velit, malesuada at elit ut, fringilla accumsan mauris. Nam lacus nibh, rhoncus eu metus non, interdum rhoncus elit. Fusce vehicula, dui et posuere malesuada, urna magna rutrum nulla, sed fringilla libero lorem vitae mauris. Mauris sagittis at lectus vitae consequat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin imperdiet dolor vel nulla dictum, eu efficitur mi sollicitudin. Morbi dictum, eros a vestibulum blandit, elit elit accumsan ex, vitae ullamcorper tellus tortor sagittis nunc. Nullam sapien sapien, lacinia nec est non, vulputate euismod erat. Proin vestibulum, felis quis feugiat tincidunt, tellus lectus vestibulum dolor, non tempor justo nisl sed erat. Fusce ac dictum lacus. Duis accumsan, nulla et sodales consectetur, dolor justo posuere nisi, et interdum elit orci eget quam. Integer interdum at neque vitae bibendum. Cras auctor, mauris et tristique vulputate, enim ante lacinia dolor, ac bibendum turpis nulla eget magna. Vivamus blandit libero at neque blandit posuere. Nullam non malesuada augue, sollicitudin rutrum leo.
+
+                Curabitur lobortis maximus quam. Praesent et turpis quis magna maximus placerat vel vel tortor. Suspendisse tempus tortor placerat dolor malesuada, bibendum viverra tellus pulvinar. Vivamus rutrum maximus erat. Morbi rutrum dui auctor orci pellentesque, at iaculis dui dapibus. Integer fermentum aliquet ullamcorper. Phasellus ullamcorper suscipit aliquam. Nullam sit amet rhoncus ex. Curabitur finibus nisl eget viverra fermentum. Vestibulum at scelerisque enim, id suscipit neque. Pellentesque vehicula iaculis vestibulum. Ut fermentum nec massa ut feugiat. Vivamus velit diam, efficitur in ligula ac, commodo pulvinar mi. Pellentesque tempus risus augue, quis rhoncus sapien vulputate ac. Maecenas ac turpis purus. Pellentesque hendrerit lacus sit amet lectus blandit dignissim.
+
+                Vestibulum porttitor eros placerat pulvinar hendrerit. Etiam convallis facilisis ex. Morbi laoreet tellus eu viverra consectetur. Nullam feugiat ultricies nulla, eleifend maximus sapien hendrerit a. Proin dignissim euismod sem, in tristique ipsum tincidunt eu. Etiam purus erat, iaculis et nibh ut, gravida finibus tortor. Nullam ullamcorper mi purus, quis tempus neque suscipit sed. Maecenas a aliquet mauris. Nulla sit amet ex eu magna eleifend blandit. Nullam dictum ultrices dapibus.
+
+                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dictum ligula ac magna mattis convallis. Ut sit amet dui at ex hendrerit cursus. Sed nec neque ullamcorper, sagittis tortor luctus, fringilla dolor. Nunc malesuada ligula malesuada venenatis dictum. Proin finibus molestie nunc, at sagittis dui porta sed. Sed luctus consectetur nibh, at condimentum tellus feugiat sed. Cras tempor consequat leo, eget consequat ligula maximus ac. Etiam finibus eget tellus at bibendum. Duis eget dictum augue. Phasellus ipsum nibh, condimentum et convallis at, congue in diam.
+
+                Phasellus a massa risus. Pellentesque accumsan, lacus quis sollicitudin rutrum, lectus lacus sollicitudin nulla, sed imperdiet lacus urna eu ipsum. In condimentum ante id dignissim viverra. Sed auctor odio metus, nec vulputate enim blandit vel. Fusce maximus enim id mattis feugiat. Quisque pharetra consequat mi porta interdum. Vivamus ac libero vitae nisi fringilla posuere vel tristique neque. Curabitur ut sem ac diam imperdiet tristique ut quis massa. Cras id ullamcorper tellus. Nulla vel elementum augue. Aliquam non efficitur metus, id gravida mi. Curabitur tincidunt quis mauris in feugiat. Vivamus ullamcorper sem a venenatis volutpat. Sed luctus eu lorem vitae faucibus.
+
+                Proin pretium vestibulum risus id rhoncus. Pellentesque ornare dui sit amet feugiat malesuada. Maecenas dapibus magna nec libero sagittis congue. Cras ipsum velit, convallis a hendrerit eu, tincidunt non lorem. Curabitur condimentum scelerisque ullamcorper. Aenean dignissim lacus et cursus suscipit. Integer molestie diam vel neque vulputate, nec laoreet quam rhoncus. Etiam tristique placerat neque, at posuere lectus tempor et. Maecenas tempus, quam id convallis malesuada, massa nisi condimentum est, in scelerisque purus ipsum cursus leo. Donec sem mi, mattis sit amet metus et, tristique varius metus.
+
+                Etiam non elit diam. Ut a libero et leo bibendum maximus vel pharetra nunc. Vivamus imperdiet metus quis mi lobortis, in bibendum quam aliquet. Sed malesuada arcu ac tellus efficitur, nec blandit purus maximus. Cras laoreet scelerisque elit, nec egestas elit fringilla et. Donec nec risus malesuada, blandit odio eu, vestibulum diam. Maecenas consectetur, lectus et sodales tempor, nibh sem fringilla nisl, vel bibendum justo ex tempus justo. Phasellus eu magna mauris. Integer fringilla sed velit eu ullamcorper. Sed in ex elit. Sed in condimentum erat, et luctus nibh. Mauris ac orci ut lectus mollis suscipit ut eget nisl. Aenean aliquam vitae erat vitae porta. Aliquam aliquet, nunc in iaculis placerat, metus diam fermentum nisl, in dapibus odio nisl vitae arcu.
+
+                Nunc hendrerit massa odio, id condimentum neque euismod hendrerit. Aliquam nec varius ligula, ac tincidunt velit. Donec non nisi pretium, porta elit ac, ultrices tellus. Integer dolor nunc, volutpat in mauris sit amet, mattis faucibus lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In hac habitasse platea dictumst. Suspendisse eu risus vitae turpis commodo ultrices. Aliquam a iaculis nunc. Sed scelerisque, lacus in suscipit posuere, ante magna suscipit elit, ac luctus ex nunc aliquet tortor. Praesent id hendrerit mi. Nulla vel ex in leo pretium tristique sit amet ut nibh. Suspendisse potenti. Integer vehicula urna lacus, vestibulum cursus turpis semper sed. Integer hendrerit ligula at facilisis molestie. Integer vestibulum congue egestas. Curabitur iaculis purus quam, eu ullamcorper massa pretium nec.
+
+                Curabitur in tristique felis, non euismod nibh. Mauris laoreet erat vitae ipsum varius maximus. Sed non metus lobortis, rutrum purus eu, auctor enim. Morbi quis auctor justo, eget varius augue. Suspendisse sagittis magna sit amet sem lobortis blandit sit amet ac lorem. Maecenas ullamcorper in odio at eleifend. Morbi feugiat odio facilisis mauris condimentum tincidunt. Proin non enim enim. Mauris commodo lectus sem, dignissim rutrum sem ultricies vel.
+            </Typography>
+        </React.Fragment>
+    )
+}
+
+export default TOC
