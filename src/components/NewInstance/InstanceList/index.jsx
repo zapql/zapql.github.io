0598414abb8dcca7a50feffdddc3d735a51ba64f
Jackson Benete <jacksonbenete@gmail.com>
2021-03-15 21:07:48 -0300

    InstanceList recovering instances

--- src/components/NewInstance/InstanceList/index.jsx
+++ src/components/NewInstance/InstanceList/index.jsx
@@ -6,24 +6,34 @@ import ListItemText from '@material-ui/core/ListItemText'
 import NightsStayIcon from '@material-ui/icons/NightsStay'
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto'
 import {Container, IList, IButton} from './style'
+import { useHistory } from 'react-router'
 
-const InstanceList = ({dispatch, STATUS}) => {
+const InstanceList = ({state, dispatch, STATUS}) => {
 
     const createNewInstance = () => {
         dispatch((previousState) => {
             return {...previousState, status: STATUS.CREATE}
         })
     }
+
+    let history = useHistory()
+
+    const redirectTo = (jwt) => {
+        return history.push("/open/"+jwt)
+    }
+    
     return (
         <Container>
             <h3>Instâncias</h3>
             <IList>
-                <ListItem button>
-                    <ListItemIcon>
-                        <NightsStayIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="INST 1" />
-                </ListItem>
+                {Object.keys(state).map((i) => (
+                    <ListItem key={i} button onClick={() => redirectTo(state[i].jwt)}>
+                        <ListItemIcon>
+                            <NightsStayIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={`[${i}] ${state[i].name}`} />
+                    </ListItem>
+                ))}
             </IList>
             <IButton onClick={() => createNewInstance()}>
                 <AddAPhotoIcon />
