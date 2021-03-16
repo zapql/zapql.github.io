36a5c62e084138c4599cabbc64dd53061ae5150f
Jackson Benete <jacksonbenete@gmail.com>
2021-03-16 16:18:50 -0300

    Improve SearchBar

--- src/components/ChatList/SearchBar/SearchBar.jsx
+++ src/components/ChatList/SearchBar/SearchBar.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import {SearchInput, SearchContainer} from './SearchBarStyle'
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Search from '@material-ui/icons/Search'
 import Divider from '@material-ui/core/Divider';
 
 const SearchBar = ({ state, dispatch }) => {
 
+    const [searchBackgroundState, setSearchBackgroundState] = useState(false)
     const onChange = ({ target }) => {
         dispatch(target.value);
-    };
+    }
+
+    const onFocus = ({ target }) => {
+        setSearchBackgroundState(true)
+    }
+
+    const onBlur = ({ target }) => {
+        setSearchBackgroundState(false)
+    }
 
     return (
         <React.Fragment>
-            <SearchContainer data-testid="SearchContainer">
+            <SearchContainer toggleBackground={state ? true : searchBackgroundState} data-testid="SearchContainer">
                 <SearchInput 
                 size="small" 
                 fullWidth 
+                disableUnderline
                 autoComplete="off"
-                id="outlined-basic" 
-                label="" 
-                placeholder="Search" 
-                variant="outlined"
+                type="text" 
+                placeholder="Search"
                 value={state || ''}
                 onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                startAdornment={
+                    <InputAdornment position="start">
+                      <Search />
+                    </InputAdornment>
+                  }
                 />
             </SearchContainer>
             <Divider />
--- src/components/ChatList/SearchBar/SearchBarStyle.js
+++ src/components/ChatList/SearchBar/SearchBarStyle.js
@@ -1,15 +1,33 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 // import Input from '@material-ui/core/Input'
 import TextField from '@material-ui/core/TextField'
+import Input from '@material-ui/core/Input'
 
 export const SearchContainer = styled.div`
-    background-color: #f6f6f6;
     padding-top: 1vh;
     padding-bottom: 1vh;
     padding-left: 2vh;
     padding-right: 2vh;
+
+${(props) =>
+    props.toggleBackground
+    ? css`
+        background-color: #FFF;`
+    : css`
+        background-color: #f6f6f6;`
+    }
 `
 
-export const SearchInput = styled(TextField)`
-    background-color: #FFF
+export const SearchInput = styled(Input)`
+    background-color: #FFF;
+    border: none;
+    border-radius: 999px;
+    padding: 0.5vh;
+    padding-left: 20px;
+    padding-right: 20px;
+    font-size: 15px;
+    outline: none;
+    box-shadow: 0 1px silver;
+    font-size: 18px;
+    line-height: 45px;
 `
